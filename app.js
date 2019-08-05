93f4d48a4046d39db16168a753bde8cd668d985b
vinaynpatil <reachvinaynp@gmail.com>
2019-08-05 17:59:04 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+
+app.use(morgan('tiny'));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'views/index.html'));
+})
+
+app.listen(3000, function(){
+    debug(`listening on port ${chalk.green('3000')}`);
+});
