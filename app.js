40247cbf42fa6827363c249f5aab5a8065c6235c
vinaynpatil <reachvinaynp@gmail.com>
2019-08-05 22:34:52 -0500

    Pug template engine usage test

--- app.js
+++ app.js
@@ -20,8 +20,12 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
+// Letting express know that we will be using a template engine
+app.set('views', './src/views/');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
