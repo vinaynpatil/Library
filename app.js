a1070e68cbf5d2e47fa10d80b703cd68c92ccca3
vinaynpatil <reachvinaynp@gmail.com>
2019-08-05 20:03:46 -0500

    Adding tooling setup

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+// process.env is a big object of all the things passed in
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
 // Saying I am setting up a static directory that I am going to use for static files (CSS and JS)
 // This means everything in public is accessible to the outside world
-app.use(express.static(path.join(__dirname, '/public/')))
+app.use(express.static(path.join(__dirname, '/public/')));
 
-// If you can't find the CSS/ JS file you are looking for in the public directory then look in the nodemodules
-app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css/')))
-app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js/')))
-app.use('/js', express.static(path.join(__dirname,'/node_modules/jquery/dist/')))
+// If you can't find the CSS/ JS file you are looking
+// for in the public directory then look in the nodemodules
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname,'views/index.html'));
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-app.listen(3000, function(){
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`Listening on port ${chalk.green(port)}`);
 });
